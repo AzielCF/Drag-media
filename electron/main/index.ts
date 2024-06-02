debee82f4b8a17cb84b2a7a43f0cac3c48551833
Aziel <azielcruzado@gmail.com>
2024-06-01 19:10:06 -0500

    Update title name to Drag media

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -55,7 +55,7 @@ const indexHtml = join(process.env.DIST, 'index.html')
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
+    title: 'Drag media',
     icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
     titleBarStyle: 'hidden',
     titleBarOverlay: {
