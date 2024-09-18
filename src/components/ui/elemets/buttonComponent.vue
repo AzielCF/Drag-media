dd32f5e873797bee2e3c5bbd4303edd03e98efad
Aziel <azielcruzado@gmail.com>
2024-09-18 13:08:09 -0500

    add hover efect

--- src/components/ui/elemets/buttonComponent.vue
+++ src/components/ui/elemets/buttonComponent.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 defineProps({
-    valueName: String
+    valueName: String,
+    disabled: Boolean
 })
 </script>
 
 <template>
-    <div
-        class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[42px] px-4 shadow-none rounded-sm cursor-pointer text-gray-50 flex items-center justify-center">
-        <div><slot name="icon"></slot></div>{{ valueName }}
-    </div>
+    <button :class="disabled ? 'opacity-40 cursor-not-allowed' : ''"
+        class="bg-[#2b2b2b] hover:bg-[#3f3f3f] h-[42px] px-4 outline-none border-none shadow-none rounded-sm cursor-pointer text-gray-50 flex items-center justify-center">
+        <div>
+            <slot name="icon"></slot>
+        </div>{{ valueName }}
+    </button>
 </template>
