965290db8aa5ca74f2731e82dc3bc79913659b95
Aziel <azielcruzado@gmail.com>
2024-11-27 17:32:56 -0500

    refactor rename app

--- src/components/customTitleBarComponent.vue
+++ src/components/customTitleBarComponent.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
 import logo from '../components/ui/icons/logo.vue'
+import uppercaseFirstLetter from '@/utils/uppercaseFirstLetter.util'
+import { useSearchStore } from '@/stores/search'
+import { storeToRefs } from 'pinia'
+
+const { selectedApiName } = storeToRefs(useSearchStore())
+
 </script>
 <template>
-    <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">         
-        <div class="flex flex-wrap items-center px-4 text-lg"><logo class="h-5 pr-2"/>Pexels drag</div>
+    <div class="drag-title-bar fixed flex h-[40px] bg-[#333333] w-screen left-0 top-0 items-center">
+        <div class="flex flex-wrap items-center px-4 text-lg">
+            <logo class="h-5 pr-2" /><span>Drag media<span v-if="selectedApiName"
+                    class="text-[#888888]"> | {{ useRoute().path != '/settings' ? uppercaseFirstLetter(selectedApiName) : 'Ajustes' }}</span></span>
+        </div>
     </div>
 </template>
 <style>
-.drag-title-bar{
+.drag-title-bar {
     -webkit-app-region: drag;
     z-index: 9999;
 }
