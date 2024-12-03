902abb92281baf5d45963fffb3671fc427ec9179
Aziel <azielcruzado@gmail.com>
2024-12-02 19:15:35 -0500

    add new exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export * from './executeOnScrollThreshold';
 export * from './load-abort-axios.util';
 export * from './convertString.util';
+export * from './uppercaseFirstLetter.util';
+export * from './getGitHubUserURL.util';
